d717d1d241c80056df084626e43144bd2c8480a4
vishu1299 <vishvasgupta129@gmail.com>
2025-03-24 12:44:15 +0530

    folder structure change2

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "https://vcom-new.vercel.app/"],
+    domains: ["images.unsplash.com", "vcom-new.vercel.app"],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
